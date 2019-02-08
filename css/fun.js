c08ca7941a399fbaf1e1761897f15cc2523255d2
Handika <dikey1988@gmail.com>
2019-02-08 08:58:20 +0700

    initial

---
+++ css/fun.js
@@ -0,0 +1,5 @@
+if (result == 0) {result2 = "I don't think you studied.<br /><img src='0.jpg' width='300' />"};
+    if (result == 25) {result2 = "You need to spend more time. Try again.<br /><img src='25.jpg' width='300' />"};
+    if (result == 50) {result2 = "I think you could do better. Try again.<br /><img src='50.jpg' width='300' />"};
+    if (result == 75) {result2 = "So close. Try again.<br /><img src='75.jpg' width='300' />"};
+    if (result == 100) {result2 = <a href="https://www.w3schools.com">Visit W3Schools</a><br /><img src='100.jpg' width='300'/>};
---
+++ js/count.js
@@ -0,0 +1,9 @@
+function clickCounter() {
+  if(typeof(Storage) !== "undefined") {
+    if (sessionStorage.clickcount) {
+      sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+    } else {
+      sessionStorage.clickcount = 1;
+    }
+  }
+}
